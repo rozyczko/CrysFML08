2485deb8de2786380d73d9482c9edc68f2eeeb26
Juan Rodriguez-Carvajal <rodriguez-carvajal@ill.fr>
2021-05-03 09:47:07 +0200

    Adding to_FPS.sh for Linux

---
+++ Testing/Programs_FP/Linux/to_FPS.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Script to compile the FullProf Suite Programs on Linux
+#
+echo "to_FPS : Adding programs to the FullProf Suite (ifort only)"
+#
+# Compiler Name
+#
+COMP="ifort"
+# "#### MHall Program ####"
+
+chmod +x make_hall.sh
+make_hall.sh $COMP
+##
+# "#### TOF_fit_LM Program ####"
+chmod +x make_tof_LM.sh
+tof_LM.sh $COMP
