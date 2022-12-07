34aa32801b6dedc40dee78d9e01c1742cac606a8
nebil a. katcho <nebil.katcho@gmail.com>
2022-12-07 16:45:27 +0100

    Second example of a calculation of a powder pattern. In this case the input is obtained from a json file

---
+++ Testing/Powder/Test_2/powder.py
@@ -0,0 +1,12 @@
+import json
+import powder_mod
+
+def load_data(fname : str) -> dict:
+
+    with open(fname) as f:
+        data = json.load(f)
+    return data
+
+def compute_pattern(data: dict):
+
+    powder_mod.simulation(data)
