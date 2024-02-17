80b546bda39aa04c42153f05959ef1ee11bef232
nebil a. katcho <nebil.katcho@gmail.com>
2024-02-17 12:03:03 +0100

    wrapper_types.py

--- Src/python_scripts/wrap_types.py
+++
@@ -1,8 +0,0 @@
-def wrap(modules) -> None:
-
-    if not os.path.isdir('CFML_Wraps'):
-        os.mkdir('CFML_Wraps')
-    for m in modules:
-        wrap_cfml_module_types(m)
-    write_cfml_wraps()
-    return None
